fc1a27bd05b8834ee9af3d6590c14be40e73f241
Chaman Vétéran <nathm.va@gmail.com>
2022-12-23 13:30:39 +0100

    Add files via upload

--- tests/placement/sans_fonction/test.ml
+++ tests/placement/sans_fonction/test.ml
@@ -44,105 +44,105 @@ let pathFichiersRat = "../../../../../tests/placement/sans_fonction/fichiersRat/
 (*  TESTS *)
 (**********)
 
-open Ast
+
 let%test "test1_x" = 
-  test (pathFichiersRat^"test1.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "x",1)  (0,"SB")
+  test (pathFichiersRat^"test1.rat")  ("main", Type.Neant) (("x", Type.Neant),1)  (0,"SB")
 
 let%test "test2_x" = 
-  test (pathFichiersRat^"test2.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "x",1)  (0,"SB")
+  test (pathFichiersRat^"test2.rat")  ("main", Type.Neant) (("x", Type.Neant),1)  (0,"SB")
 
 let%test "test2_y" = 
-  test (pathFichiersRat^"test2.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "y",1) (1,"SB")
+  test (pathFichiersRat^"test2.rat")  ("main", Type.Neant) (("y", Type.Neant),1) (1,"SB")
 
 let%test "test2_z" = 
-  test (pathFichiersRat^"test2.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "z",1)  (2 ,"SB")
+  test (pathFichiersRat^"test2.rat")  ("main", Type.Neant) (("z", Type.Neant),1)  (2 ,"SB")
 
 let%test "test3_x" = 
-  test (pathFichiersRat^"test3.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "x",1)  (0, "SB")
+  test (pathFichiersRat^"test3.rat")  ("main", Type.Neant) (("x", Type.Neant),1)  (0, "SB")
 
 let%test "test3_y" = 
-  test (pathFichiersRat^"test3.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "y",1)  (1, "SB")
+  test (pathFichiersRat^"test3.rat")  ("main", Type.Neant) (("y", Type.Neant),1)  (1, "SB")
 
 let%test "test3_z" = 
-  test (pathFichiersRat^"test3.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "z",1)  (2, "SB")
+  test (pathFichiersRat^"test3.rat")  ("main", Type.Neant) (("z", Type.Neant),1)  (2, "SB")
 
 let%test "test4_x" = 
-  test (pathFichiersRat^"test4.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "x",1)  (0, "SB")
+  test (pathFichiersRat^"test4.rat")  ("main", Type.Neant) (("x", Type.Neant),1)  (0, "SB")
   
 let%test "test4_y" = 
-  test (pathFichiersRat^"test4.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "y",1)  (2, "SB")
+  test (pathFichiersRat^"test4.rat")  ("main", Type.Neant) (("y", Type.Neant),1)  (2, "SB")
   
 let%test "test4_z" = 
-  test (pathFichiersRat^"test4.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "z",1)  (4, "SB")
+  test (pathFichiersRat^"test4.rat")  ("main", Type.Neant) (("z", Type.Neant),1)  (4, "SB")
 
 let%test "test5_x" = 
-  test (pathFichiersRat^"test5.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "x",1)  (0, "SB")
+  test (pathFichiersRat^"test5.rat")  ("main", Type.Neant) (("x", Type.Neant),1)  (0, "SB")
   
 let%test "test5_y" = 
-  test (pathFichiersRat^"test5.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "y",1)  (1, "SB")
+  test (pathFichiersRat^"test5.rat")  ("main", Type.Neant) (("y", Type.Neant),1)  (1, "SB")
   
 let%test "test5_z" = 
-  test (pathFichiersRat^"test5.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "z",1)  (3, "SB")
+  test (pathFichiersRat^"test5.rat")  ("main", Type.Neant) (("z", Type.Neant),1)  (3, "SB")
 
 let%test "test6_x_1" = 
-  test (pathFichiersRat^"test6.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "x",1)  (0, "SB")
+  test (pathFichiersRat^"test6.rat")  ("main", Type.Neant) (("x", Type.Neant),1)  (0, "SB")
   
 let%test "test6_y_1" = 
-  test (pathFichiersRat^"test6.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "y",1)  (1, "SB")
+  test (pathFichiersRat^"test6.rat")  ("main", Type.Neant) (("y", Type.Neant),1)  (1, "SB")
   
 let%test "test6_z_1" = 
-  test (pathFichiersRat^"test6.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "z",1)  (3, "SB")
+  test (pathFichiersRat^"test6.rat")  ("main", Type.Neant) (("z", Type.Neant),1)  (3, "SB")
 
 let%test "test6_x_2" = 
-  test (pathFichiersRat^"test6.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "x",2)  (4, "SB")
+  test (pathFichiersRat^"test6.rat")  ("main", Type.Neant) (("x", Type.Neant),2)  (4, "SB")
   
 let%test "test6_y_2" = 
-  test (pathFichiersRat^"test6.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "y",2)  (5, "SB")
+  test (pathFichiersRat^"test6.rat")  ("main", Type.Neant) (("y", Type.Neant),2)  (5, "SB")
   
 let%test "test6_z_2" = 
-  test (pathFichiersRat^"test6.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "z",2)  (7, "SB")
+  test (pathFichiersRat^"test6.rat")  ("main", Type.Neant) (("z", Type.Neant),2)  (7, "SB")
 
 let%test "test6_x_3" = 
-  test (pathFichiersRat^"test6.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "x",3)  (4, "SB")
+  test (pathFichiersRat^"test6.rat")  ("main", Type.Neant) (("x", Type.Neant),3)  (4, "SB")
   
 let%test "test6_y_3" = 
-  test (pathFichiersRat^"test6.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "y",3)  (5, "SB")
+  test (pathFichiersRat^"test6.rat")  ("main", Type.Neant) (("y", Type.Neant),3)  (5, "SB")
   
 let%test "test6_z_3" = 
-  test (pathFichiersRat^"test6.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "z",3)  (7, "SB")
+  test (pathFichiersRat^"test6.rat")  ("main", Type.Neant) (("z", Type.Neant),3)  (7, "SB")
 
 let%test "test6_x1" = 
-  test (pathFichiersRat^"test6.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "x1",1)  (4, "SB")
+  test (pathFichiersRat^"test6.rat")  ("main", Type.Neant) (("x1", Type.Neant),1)  (4, "SB")
   
 let%test "test6_y1" = 
-  test (pathFichiersRat^"test6.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "y1",1)  (5, "SB")
+  test (pathFichiersRat^"test6.rat")  ("main", Type.Neant) (("y1", Type.Neant),1)  (5, "SB")
   
 let%test "test6_z1" = 
-  test (pathFichiersRat^"test6.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "z1",1)  (7, "SB")
+  test (pathFichiersRat^"test6.rat")  ("main", Type.Neant) (("z1", Type.Neant),1)  (7, "SB")
 
 let%test "test7_x_1" = 
-  test (pathFichiersRat^"test7.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "x",1)  (0, "SB")
+  test (pathFichiersRat^"test7.rat")  ("main", Type.Neant) (("x", Type.Neant),1)  (0, "SB")
     
 let%test "test7_y_1" = 
-  test (pathFichiersRat^"test7.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "y",1)  (1, "SB")
+  test (pathFichiersRat^"test7.rat")  ("main", Type.Neant) (("y", Type.Neant),1)  (1, "SB")
     
 let%test "test7_z_1" = 
-  test (pathFichiersRat^"test7.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "z",1)  (3, "SB")
+  test (pathFichiersRat^"test7.rat")  ("main", Type.Neant) (("z", Type.Neant),1)  (3, "SB")
   
 let%test "test7_x_2" = 
-  test (pathFichiersRat^"test7.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "x",2)  (4, "SB")
+  test (pathFichiersRat^"test7.rat")  ("main", Type.Neant) (("x", Type.Neant),2)  (4, "SB")
     
 let%test "test7_y_2" = 
-  test (pathFichiersRat^"test7.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "y",2)  (5, "SB")
+  test (pathFichiersRat^"test7.rat")  ("main", Type.Neant) (("y", Type.Neant),2)  (5, "SB")
     
 let%test "test7_z_2" = 
-  test (pathFichiersRat^"test7.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "z",2)  (7, "SB")
+  test (pathFichiersRat^"test7.rat")  ("main", Type.Neant) (("z", Type.Neant),2)  (7, "SB")
   
 let%test "test7_x1" = 
-  test (pathFichiersRat^"test7.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "x1",1)  (4, "SB")
+  test (pathFichiersRat^"test7.rat")  ("main", Type.Neant) (("x1", Type.Neant),1)  (4, "SB")
     
 let%test "test7_y1" = 
-  test (pathFichiersRat^"test7.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "y1",1)  (5, "SB")
+  test (pathFichiersRat^"test7.rat")  ("main", Type.Neant) (("y1", Type.Neant),1)  (5, "SB")
     
 let%test "test7_z1" = 
-  test (pathFichiersRat^"test7.rat")  (AstSyntax.Symbole "main") (AstSyntax.Symbole "z1",1)  (7, "SB")
+  test (pathFichiersRat^"test7.rat")  ("main", Type.Neant) (("z1", Type.Neant),1)  (7, "SB")
