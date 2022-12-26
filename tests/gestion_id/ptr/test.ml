e54c00e51aec50bff2ab24040395c56335e91934
Chaman Vétéran <nathm.va@gmail.com>
2022-12-26 15:35:40 +0100

    Add tests ptr id

---
+++ tests/gestion_id/ptr/test.ml
@@ -0,0 +1,21 @@
+open Rat
+open Compilateur
+(*open Exceptions*)
+open Exceptions_identifiants
+
+exception ErreurNonDetectee
+let pathFichiersRat = "./fichiersRat/"
+
+(**********)
+(*  TESTS *)
+(**********)
+
+let%test_unit "testAffectation1"= 
+    let _ = compiler (pathFichiersRat^"testAffectation1.rat") in ()
+let%test_unit "testAffectation2"= 
+    let _ = compiler (pathFichiersRat^"testAffectation2.rat") in ()
+let%test_unit "testDblDeclaration"= 
+    try
+      let _ = compiler (pathFichiersRat^"testDblDeclaration.rat")
+      in raise ErreurNonDetectee
+    with DoubleDeclaration "x" -> ()
